25ebc26fedbfd0433d1d95b62417f3b82dfd0783
villiamriegler <74962511+villiamriegler@users.noreply.github.com>
2021-03-08 09:27:34 +0100

    Derivata plots
    plottar derivatans graf

--- plots.py
+++ plots.py
@@ -1,19 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from NumericDerivative import *
 
 
-f = input('Ange en funktion: ')
-y = lambda x: eval(f)
+y = input('Ange en funktion: ')
+f = lambda x: eval(y)
 
 xpoints = np.array([])
 ypoints = np.array([])
-for x in range(-500,500):
+dfpoints = np.array([])
+for x in range(-50,50):
     xpoints = np.append(xpoints,x)
-    ypoints = np.append(ypoints,y(x))
+    ypoints = np.append(ypoints,f(x))
+    dfpoints = np.append(dfpoints, derivative(f,x)) 
 
 
 
 
 plt.plot(xpoints, ypoints)
+plt.plot(xpoints, dfpoints)
 plt.grid()
 plt.show()
